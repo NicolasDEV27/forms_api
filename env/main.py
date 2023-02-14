cada5ed2c9024feefd6e8418e367d333a4e7b37d
Nicolas <devnicolas27@gmail.com>
2023-02-14 17:50:53 -0500

    First proof commit

---
+++ env/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI
+app.title = 'FORMS API'
+app.version = '0.0.1'
+
+@app.get('/', tags=['Home'])
+def message():
+    return HTMLResponse('Hello World')
