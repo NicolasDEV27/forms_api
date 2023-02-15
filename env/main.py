2592f2fb71a8ee81d7b18fa9ee05fdede3947cdb
Nicolas <devnicolas27@gmail.com>
2023-02-15 14:02:16 -0500

    Add new modules and little changes

--- env/main.py
+++ env/main.py
@@ -7,7 +7,7 @@ app.version = '0.0.1'
 
 @app.get('/', tags=['Home'])
 def message():
-    return HTMLResponse('Hello world')
+    return HTMLResponse('Todays date')
 
 @app.post("/login/")
 def login(username: str = Form(), password: str = Form()):
